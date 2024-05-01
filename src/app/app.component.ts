c2e6f888af573f40191a2b2a900b2db5683fd572
mehmetKargaci <mehmetkargaci34@gmail.com>
2024-05-01 23:51:43 +0200

    final html and  css updating

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,82 +1,82 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup , Validators} from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent implements OnInit{
+export class AppComponent implements OnInit {
 
-  inputControl =  new FormControl('');
-  buttons = ['+', '-', '*', '/','%', '7', '8','9', '4','5','6','1','2','3','0','.','%', 'Delete all', '=']
-  errormessage= false;
+  inputControl = new FormControl('');
+  buttons = ['7', '8', '9', '+', '4', '5', '6', '-', '1', '2', '3', '*', '0', '.', '%','/']
+  errormessage = false;
 
-  result!: number;
-  results: {operation: string, result: number}[] = [];
+  result: number = 0;
+  results: { operation: string, result: number }[] = [];
 
-  constructor(){ }
+  constructor() { }
 
   ngOnInit(): void {
-  } 
+  }
 
-  pressNum(num: string){   
+  pressNum(num: string) {
     const value = this.inputControl.value;
     this.inputControl.setValue(value + num);
-  } 
+  }
 
-  deleteAll(){
+  deleteAll() {
     this.inputControl.setValue('');
     this.errormessage = true;
-    
+    this.results = [];
   }
-  clear(){    
-  
+
+  clear() {
+    this.inputControl.setValue('');
   }
 
   calculate() {
     const value = this.inputControl.value;
-    const opeartors = ['+', '-', '*', '/','%'];
-    
+    const opeartors = ['+', '-', '*', '/', '%'];
+
     // Find operator
-    for (const operator of opeartors){
+    for (const operator of opeartors) {
       const index = value?.indexOf(operator) || -1;
-      if (index > -1){
+      if (index > -1) {
+        console.log(this.results)
         const firstNumber = value?.slice(0, index) || '';
-        const secondNumber = value?.slice(index+1) || '';
+        const secondNumber = value?.slice(index + 1) || '';
         console.log(firstNumber, operator, secondNumber);
         //add operator
-        if(operator === '+'){
-          this.result = parseFloat(firstNumber) + parseFloat(secondNumber) ;
-        }
-        //substruct operator
-        if(operator === '-'){
-          this.result = parseFloat(firstNumber) - parseFloat(secondNumber) ;
-        }
-        //multiple operator
-        if(operator === '*'){
-          this.result = parseFloat(firstNumber) * parseFloat(secondNumber) ;
-        }
-        //divide operator
-        if(operator === '/'){
-          this.result = parseFloat(firstNumber) / parseFloat(secondNumber) ;
-        }
-        //percentage operator
-        if(operator === '%'){
-          this.result = parseFloat(firstNumber) / 100 ;
-        }
-
-        
-        if (isNaN(Number(firstNumber)) || isNaN(+secondNumber) && (operator !== '+' || '-' || '*'||'/'||'%')){
+        if (isNaN(Number(firstNumber)) || isNaN(+secondNumber) && (operator !== '+' || '-' || '*' || '/' || '%')) {
           console.log('Invalid value');
           this.errormessage = true;
+        } else {
+          this.errormessage = false;
+          if (operator === '+') {
+            this.result = parseFloat(firstNumber) + parseFloat(secondNumber);
+          }
+          //substruct operator
+          if (operator === '-') {
+            this.result = parseFloat(firstNumber) - parseFloat(secondNumber);
+          }
+          //multiple operator
+          if (operator === '*') {
+            this.result = parseFloat(firstNumber) * parseFloat(secondNumber);
+          }
+          //divide operator
+          if (operator === '/') {
+            this.result = parseFloat(firstNumber) / parseFloat(secondNumber);
+          }
+          //percentage operator
+          if (operator === '%') {
+            this.result = parseFloat(firstNumber) / 100;
+          }
+          this.results.push({ operation: value || '', result: this.result });
+          this.inputControl.setValue(this.result.toString())
         }
-        if(this.errormessage===false){
-          this.results.push({ operation: value || '', result: this.result})
-          break;         
-        }
-      
-
+        console.log(this.results)
+        break;
       }
     }
 
@@ -87,10 +87,10 @@ export class AppComponent implements OnInit{
   }
 }
 
- 
 
 
 
 
-  
+
+
 
