e591706ac69e599c18225ab0f17179915788e818
mehmetKargaci <mehmetkargaci34@gmail.com>
2024-05-01 03:57:20 +0200

    css style and typescript updated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,88 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup , Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
-  title = 'calculator2';
+export class AppComponent implements OnInit{
+
+  inputControl =  new FormControl('');
+  errormessage= false;
+
+  result!: number;
+  results: {operation: string, result: number}[] = [];
+
+  constructor(){ }
+
+  ngOnInit(): void {
+  } 
+
+  pressNum(num: string){   
+    const value = this.inputControl.value;
+    this.inputControl.setValue(value + num);
+  } 
+
+  deleteAll(){
+    this.inputControl.setValue('');
+  }
+  clear(){    
+  
+  }
+
+  calculate() {
+    const value = this.inputControl.value;
+    const opeartors = ['+', '-', '*', '/','%'];
+    
+    // Find operator
+    for (const operator of opeartors){
+      const index = value?.indexOf(operator) || -1;
+      if (index > -1){
+        const firstNumber = value?.slice(0, index) || '';
+        const secondNumber = value?.slice(index+1) || '';
+        console.log(firstNumber, operator, secondNumber);
+        //add operator
+        if(operator === '+'){
+          this.result = parseFloat(firstNumber) + parseFloat(secondNumber) ;
+        }
+        //substruct operator
+        if(operator === '-'){
+          this.result = parseFloat(firstNumber) - parseFloat(secondNumber) ;
+        }
+        //multiple operator
+        if(operator === '*'){
+          this.result = parseFloat(firstNumber) * parseFloat(secondNumber) ;
+        }
+        //divide operator
+        if(operator === '/'){
+          this.result = parseFloat(firstNumber) / parseFloat(secondNumber) ;
+        }
+        //percentage operator
+        if(operator === '%'){
+          this.result = parseFloat(firstNumber) / 100 ;
+        }
+
+
+        if (isNaN(Number(firstNumber)) || isNaN(+secondNumber)){
+          console.log('Invalid value')
+        }
+        this.results.push({ operation: value || '', result: this.result})
+        break;
+      }
+    }
+
+    // First value
+    // Second value
+    // Validation
+    // Result
+  }
 }
+
+ 
+
+
+
+
+  
+
